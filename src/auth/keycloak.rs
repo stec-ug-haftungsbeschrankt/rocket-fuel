a4f1f30293bc6be271cb9e84d090e434ae83b4ea
Stefan Schick <stefan.schick@stecug.de>
2021-10-08 21:14:17 +0200

    Cleanup auth providers

--- src/auth/keycloak.rs
+++ src/auth/keycloak.rs
@@ -1,12 +1,10 @@
-use anyhow::{Context, Error};
+use anyhow::{Context};
 use reqwest::header::{AUTHORIZATION, USER_AGENT};
 use rocket::{http::{Cookie, CookieJar, SameSite}, response::{Debug, Redirect}};
 use rocket_oauth2::{OAuth2, TokenResponse};
 
 use crate::auth::User;
-
-
-
+use super::{Auth, UserInfo};
 
 
 /// User information to be retrieved from the Keycloak API.
@@ -15,53 +13,59 @@ pub struct KeycloakUserInfo {
 }
 
 
-#[get("/auth/login/keycloak")]
-pub fn keycloak_login(oauth2: OAuth2<KeycloakUserInfo>, cookies: &CookieJar<'_>) -> Redirect {
-    oauth2.get_redirect(cookies, &["openid", "profile", "email"]).unwrap()
-}
+impl UserInfo for KeycloakUserInfo {
 
-#[get("/auth/keycloak")]
-pub async fn keycloak_callback(token: TokenResponse<KeycloakUserInfo>, cookies: &CookieJar<'_>) -> Result<Redirect, Debug<Error>> {
-    let user_info: User = reqwest::Client::builder()
-        .build()
-        .context("failed to build reqwest client")?
-        .get("https://auth.stecgmbh.de/auth/realms/stec/protocol/openid-connect/userinfo")
-        .header(AUTHORIZATION, format!("Bearer {}", token.access_token()))
-        .header(USER_AGENT, "STEC Monitoring")
-        .send()
-        .await
-        .context("failed to complete request")?
-        .json()
-        .await
-        .context("failed to deserialize response")?;
-
-    let data: String = user_info.into();
-
-    cookies.add_private(
-        Cookie::build("user", data)
-            .same_site(SameSite::Lax)
-            .finish(),
-    );
-    Ok(Redirect::to("/"))
 }
 
 
-#[get("/auth/logout/keycloak")]
-pub fn keycloak_logout(oauth2: OAuth2<KeycloakUserInfo>, cookies: &CookieJar<'_>) -> Redirect {
-    cookies.remove(Cookie::named("user"));
-    // Redirect::to("/") FIXME Where does the redirect go
-    Redirect::to("https://auth.stecgmbh.de/auth/realms/stec/protocol/openid-connect/logout")
-}
+struct Keycloak { }
 
 
-#[get("/auth/register/keycloak")]
-pub fn keycloak_register(oauth2: OAuth2<KeycloakUserInfo>, cookies: &CookieJar<'_>) -> Redirect {
-    //Redirect::to("https://auth.stecgmbh.de/auth/realms/stec/protocol/openid-connect/register")
-    //oauth2.get_redirect(cookies, &["openid", "email"]).unwrap()
+#[async_trait]
+impl Auth<KeycloakUserInfo> for Keycloak {
 
-    // FIXME Extend JebRosens auth crate for registration page
-    Redirect::to("https://auth.stecgmbh.de/auth/realms/stec/protocol/openid-connect/registrations?client_id=stec_general&response_type=code&scope=openid%20email&redirect_uri=http://localhost:8000/auth/keycloak&kc_locale=en")
+    fn login(oauth2: OAuth2<KeycloakUserInfo>, cookies: &CookieJar<'_>) -> Redirect {
+        oauth2.get_redirect(cookies, &["openid", "profile", "email"]).unwrap()
+    }
 
-    // See https://stackoverflow.com/questions/51514437/keycloak-direct-user-link-registration
-    // http://<domain.com>/auth/realms/<realm-name>/protocol/openid-connect/registrations?client_id=<client_id>&response_type=code&scope=openid email&redirect_uri=http://<domain.com>/<redirect-path>&kc_locale=<two-digit-lang-code>
+    fn logout(_oauth2: OAuth2<KeycloakUserInfo>, cookies: &CookieJar<'_>) -> Redirect {
+        cookies.remove(Cookie::named("user"));
+        // Redirect::to("/") FIXME Where does the redirect go
+        Redirect::to("https://auth.stecgmbh.de/auth/realms/stec/protocol/openid-connect/logout")
+    }
+
+    async fn callback(token: TokenResponse<KeycloakUserInfo>, cookies: &CookieJar<'_>) -> Result<Redirect, Debug<anyhow::Error>> {
+        let user_info: User = reqwest::Client::builder()
+            .build()
+            .context("failed to build reqwest client")?
+            .get("https://auth.stecgmbh.de/auth/realms/stec/protocol/openid-connect/userinfo")
+            .header(AUTHORIZATION, format!("Bearer {}", token.access_token()))
+            .header(USER_AGENT, "STEC Monitoring")
+            .send()
+            .await
+            .context("failed to complete request")?
+            .json()
+            .await
+            .context("failed to deserialize response")?;
+
+        let data: String = user_info.into();
+
+        cookies.add_private(
+            Cookie::build("user", data)
+                .same_site(SameSite::Lax)
+                .finish(),
+        );
+        Ok(Redirect::to("/"))
+    }
+
+    fn register(_oauth2: OAuth2<KeycloakUserInfo>, _cookies: &CookieJar<'_>) -> Redirect {
+        //Redirect::to("https://auth.stecgmbh.de/auth/realms/stec/protocol/openid-connect/register")
+        //oauth2.get_redirect(cookies, &["openid", "email"]).unwrap()
+
+        // FIXME Extend JebRosens auth crate for registration page
+        Redirect::to("https://auth.stecgmbh.de/auth/realms/stec/protocol/openid-connect/registrations?client_id=stec_general&response_type=code&scope=openid%20email&redirect_uri=http://localhost:8000/auth/keycloak&kc_locale=en")
+
+        // See https://stackoverflow.com/questions/51514437/keycloak-direct-user-link-registration
+        // http://<domain.com>/auth/realms/<realm-name>/protocol/openid-connect/registrations?client_id=<client_id>&response_type=code&scope=openid email&redirect_uri=http://<domain.com>/<redirect-path>&kc_locale=<two-digit-lang-code>
+    }
 }
--- src/auth/microsoft.rs
+++ src/auth/microsoft.rs
@@ -1,8 +1,9 @@
-use anyhow::{Context, Error};
+use anyhow::{Context};
 use reqwest::header::{AUTHORIZATION};
 use rocket::{http::{Cookie, CookieJar, SameSite}, response::{Debug, Redirect}};
 use rocket_oauth2::{OAuth2, TokenResponse};
 
+use super::{Auth, UserInfo};
 
 
 /// User information to be retrieved from the Microsoft API.
@@ -12,36 +13,51 @@ pub struct MicrosoftUserInfo {
     display_name: String,
 }
 
-#[get("/login/microsoft")]
-pub fn microsoft_login(oauth2: OAuth2<MicrosoftUserInfo>, cookies: &CookieJar<'_>) -> Redirect {
-    oauth2.get_redirect(cookies, &["user.read"]).unwrap()
+
+impl UserInfo for MicrosoftUserInfo {
+
 }
 
-#[get("/auth/microsoft")]
-pub async fn microsoft_callback(
-    token: TokenResponse<MicrosoftUserInfo>,
-    cookies: &CookieJar<'_>,
-) -> Result<Redirect, Debug<Error>> {
-    // Use the token to retrieve the user's Microsoft account information.
-    let user_info: MicrosoftUserInfo = reqwest::Client::builder()
-        .build()
-        .context("failed to build reqwest client")?
-        .get("https://graph.microsoft.com/v1.0/me")
-        .header(AUTHORIZATION, format!("Bearer {}", token.access_token()))
-        .send()
-        .await
-        .context("failed to complete request")?
-        .json()
-        .await
-        .context("failed to deserialize response")?;
 
-    // Set a private cookie with the user's name, and redirect to the home page.
-    cookies.add_private(
-        Cookie::build("username", user_info.display_name)
-            .same_site(SameSite::Lax)
-            .finish(),
-    );
-    Ok(Redirect::to("/"))
+struct Microsoft { }
+
+
+#[async_trait]
+impl Auth<MicrosoftUserInfo> for Microsoft {
+    fn login(oauth2: OAuth2<MicrosoftUserInfo>, cookies: &CookieJar<'_>) -> Redirect {
+        oauth2.get_redirect(cookies, &["user.read"]).unwrap()
+    }
+
+    fn logout(_oauth2: OAuth2<MicrosoftUserInfo>, _cookies: &CookieJar<'_>) -> Redirect {
+        todo!()
+    }
+
+    async fn callback(token: TokenResponse<MicrosoftUserInfo>, cookies: &CookieJar<'_>) -> Result<Redirect, Debug<anyhow::Error>> {
+        // Use the token to retrieve the user's Microsoft account information.
+        let user_info: MicrosoftUserInfo = reqwest::Client::builder()
+            .build()
+            .context("failed to build reqwest client")?
+            .get("https://graph.microsoft.com/v1.0/me")
+            .header(AUTHORIZATION, format!("Bearer {}", token.access_token()))
+            .send()
+            .await
+            .context("failed to complete request")?
+            .json()
+            .await
+            .context("failed to deserialize response")?;
+
+        // Set a private cookie with the user's name, and redirect to the home page.
+        cookies.add_private(
+            Cookie::build("username", user_info.display_name)
+                .same_site(SameSite::Lax)
+                .finish(),
+        );
+        Ok(Redirect::to("/"))
+    }
+
+    fn register(_oauth2: OAuth2<MicrosoftUserInfo>, _cookies: &CookieJar<'_>) -> Redirect {
+        todo!()
+    }
+
 }
-
 
